65f948eb33c62d21f6cef2139dafa5602f049596
berlliyanto <126399467+berlliyanto@users.noreply.github.com>
2023-03-12 17:39:15 +0700

    bot whatassp

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const {Client} = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const {Configuration, OpenAIApi} = require("openai");
+require("dotenv").config();
+const client = new Client();
+
 const mongoose = require("mongoose");
 const {MONGO_DB_CONFIG} = require("./config/app.config");
 
@@ -27,20 +33,26 @@ auth.authenticateToken.unless = unless;
 app.use(
     auth.authenticateToken.unless({
         path:[
+            //AUTH
             {url: "/api/login", methods: ["POST"]},
             {url: "/api/register", methods: ["POST"]},
+            //USERS
             {url: "/api/users", methods: ["POST"]},
             {url: "/api/users/:id", methods: ["PUT"]},
             {url: "/api/users/:id", methods: ["DELETE"]},
+            //PARAMETER
             {url: "/api/inputParam", methods: ["POST"]},
             {url: "/api/resetParamM1", methods: ["PUT"]},
             {url: "/api/deleteParam", methods: ["DELETE"]},
+            //STOCK
             {url: "/api/inputStock", methods: ["POST"]},
             {url: "/api/deleteStock", methods: ["DELETE"]},
             {url: "/api/addStockM1", methods: ["PUT"]},
+            //STATUS
             {url: "/api/insertStat", methods: ["POST"]},
             {url: "/api/statusM1", methods: ["PUT"]},
             {url: "/api/statusM2", methods: ["PUT"]},
+            //ENERGY
             {url: "/api/insertEnergy", methods: ["POST"]},
         ],
     })
@@ -55,3 +67,33 @@ app.listen(Port, function(){
     console.log("Connected to : ", Port)    
 });
 
+//WHATSAPP BOT
+client.on('qr',(qr)=>{
+    qrcode.generate(qr,{small:true});
+});
+
+client.on('ready',()=>{
+    console.log("Client is ready");
+});
+
+client.initialize();
+
+const configuration = new Configuration({
+    apiKey: process.env.SECRET_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function runCompletion(message){
+    const completion  = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: message,
+        max_tokens: 200,
+    });
+    return completion.data.choices[0].text;
+}
+
+client.on('message',message=>{
+    console.log(message.body);
+    runCompletion(message.body).then(result => message.reply(result));
+})
