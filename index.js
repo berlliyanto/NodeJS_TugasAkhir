d1c9095022d0b9545fa722b60807ce7b330735eb
berlliyanto <126399467+berlliyanto@users.noreply.github.com>
2023-04-02 11:46:34 +0700

    matikan wa bot

--- index.js
+++ index.js
@@ -90,76 +90,76 @@ app.listen(Port, function () {
 
 //--------------------------------------------------------NOTIFIKASI--------------------------------------------------------------//
 
-const { Client } = require("whatsapp-web.js");
-const qrcode = require("qrcode-terminal");
-const client = new Client();
+// const { Client } = require("whatsapp-web.js");
+// const qrcode = require("qrcode-terminal");
+// const client = new Client();
 
-const User = require('./models/auth.model');
-const { availability } = require('./models/oee.model');
-const { parameter } = require('./models/param.model');
+// const User = require('./models/auth.model');
+// const { availability } = require('./models/oee.model');
+// const { parameter } = require('./models/param.model');
 
-const availabilityStream = availability.watch();
+// const availabilityStream = availability.watch();
 
-//--------------------------------------------WHATSAPP BOT CLIENT-----------------------------------------------//
+// //--------------------------------------------WHATSAPP BOT CLIENT-----------------------------------------------//
 
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-});
+// client.on('qr', (qr) => {
+//     qrcode.generate(qr, { small: true });
+// });
 
-client.on('ready', () => {
-    sendMessages(phoneUser,"Hallo Pengguna, Kami Siap Membantu");
-    console.log("Client is ready");
-});
+// client.on('ready', () => {
+//     sendMessages(phoneUser,"Hallo Pengguna, Kami Siap Membantu");
+//     console.log("Client is ready");
+// });
 
-client.on('message', async (msg) => {
-    if (msg.body === '!ping') {
-        msg.reply('pong');
-    }
-})
+// client.on('message', async (msg) => {
+//     if (msg.body === '!ping') {
+//         msg.reply('pong');
+//     }
+// })
 
-client.initialize();
+// client.initialize();
 
-//NO HANDPHONE TERDAFTAR
-let phoneUser = [];
-setInterval(phone,5000);
-async function phone() {
-    const FetchHP = await User.find({});
-    FetchHP.forEach(function (doc) {
-        const numberPhone = doc.noHp;
-        if (!phoneUser.includes(numberPhone)) {
-            phoneUser.push(numberPhone);
-        }
-    },
-    )
-}
+// //NO HANDPHONE TERDAFTAR
+// let phoneUser = [];
+// setInterval(phone,5000);
+// async function phone() {
+//     const FetchHP = await User.find({});
+//     FetchHP.forEach(function (doc) {
+//         const numberPhone = doc.noHp;
+//         if (!phoneUser.includes(numberPhone)) {
+//             phoneUser.push(numberPhone);
+//         }
+//     },
+//     )
+// }
 
-//SEND MESSAGES TO CONTACTS
-const sendMessages = async (phone, message) => {
-    try {
-        const promises = phone.map(number => client.sendMessage(`${number}@c.us`, message));
-        await Promise.all(promises);
-        console.log(`Message "${message}" sent to ${phone.length} contacts`);
-    } catch (err) {
-        console.error('Error sending message:', err);
-    }
-};
+// //SEND MESSAGES TO CONTACTS
+// const sendMessages = async (phone, message) => {
+//     try {
+//         const promises = phone.map(number => client.sendMessage(`${number}@c.us`, message));
+//         await Promise.all(promises);
+//         console.log(`Message "${message}" sent to ${phone.length} contacts`);
+//     } catch (err) {
+//         console.error('Error sending message:', err);
+//     }
+// };
 
-availabilityStream.on('change', async (change) => {
-    if (change.operationType === 'update') {
-        const paramM1 = await parameter.findOne({ machine_id: 1 }).sort({ _id: -1 });
-        const paramM2 = await parameter.findOne({ machine_id: 2 }).sort({ _id: -1 });
-        const paramM3 = await parameter.findOne({ machine_id: 3 }).sort({ _id: -1 });
-        const paramM4 = await parameter.findOne({ machine_id: 4 }).sort({ _id: -1 });
+// availabilityStream.on('change', async (change) => {
+//     if (change.operationType === 'update') {
+//         const paramM1 = await parameter.findOne({ machine_id: 1 }).sort({ _id: -1 });
+//         const paramM2 = await parameter.findOne({ machine_id: 2 }).sort({ _id: -1 });
+//         const paramM3 = await parameter.findOne({ machine_id: 3 }).sort({ _id: -1 });
+//         const paramM4 = await parameter.findOne({ machine_id: 4 }).sort({ _id: -1 });
 
-        const docM1 = await availability.findOne({ machine_id: 1 }).sort({ _id: -1 });
-        const docM2 = await availability.findOne({ machine_id: 2 }).sort({ _id: -1 });
-        const docM3 = await availability.findOne({ machine_id: 3 }).sort({ _id: -1 });
-        const docM4 = await availability.findOne({ machine_id: 4 }).sort({ _id: -1 });
+//         const docM1 = await availability.findOne({ machine_id: 1 }).sort({ _id: -1 });
+//         const docM2 = await availability.findOne({ machine_id: 2 }).sort({ _id: -1 });
+//         const docM3 = await availability.findOne({ machine_id: 3 }).sort({ _id: -1 });
+//         const docM4 = await availability.findOne({ machine_id: 4 }).sort({ _id: -1 });
 
-        if (docM1 && docM1.runningtime >= (paramM1.loading_time) * 60) {
-            const phone = phoneUser; // daftar nomor yang ingin diberi tahu
-            const message = `Mesin 1 Selesai Beroperasi!`;
-            await sendMessages(phone, message);
-        }
-    }
-});
+//         if (docM1 && docM1.runningtime >= (paramM1.loading_time) * 60) {
+//             const phone = phoneUser; // daftar nomor yang ingin diberi tahu
+//             const message = `Mesin 1 Selesai Beroperasi!`;
+//             await sendMessages(phone, message);
+//         }
+//     }
+// });
