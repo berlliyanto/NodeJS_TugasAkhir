6f6b94cb61451143b608ec81e929edd98481a5cc
berlliyanto <126399467+berlliyanto@users.noreply.github.com>
2023-04-07 17:25:26 +0700

    availibility beres?

--- service/availability.services.js
+++ service/availability.services.js
@@ -70,17 +70,24 @@ function runAllFunctions() {
     fetchP2();
     fetchP3();
     fetchP4();
-    OpTimeM1();
-    OpTimeM2();
-    OpTimeM3();
-    OpTimeM4();
 }
 
 let interval = setInterval(runAllFunctions, 1000);
+let intervalOP;
+setTimeout(() => {
+    function runQueryOP(){
+        OpTimeM1();
+        OpTimeM2();
+        OpTimeM3();
+        OpTimeM4();
+    }
+    intervalOP = setInterval(runQueryOP,1000);
+}, 1500);
 
 //CLEAR INTERVAL
 setTimeout(function () {
     clearInterval(interval);
+    clearInterval(intervalOP);
     console.log("clear")
 }, 1000000);
 
@@ -199,9 +206,8 @@ async function OpTimeM1() {
                     $set: {
                         state: 0
                     }
-                }).sort({ _id: -1 }).then(() => {
-                    console.log("MESIN 1 SELESAI");
-                });
+                }).sort({ _id: -1 });
+                console.log("MESIN 1 SELESAI");
             }
         } else {
             return null;
@@ -230,45 +236,48 @@ async function OpTimeM2() {
             if (Time2 < (loadingM2 * 60)) {
                 Time2++;
                 if (statusM2 == 1) {
+                    TimeplusO2++
                     await availability.findOneAndUpdate({
                         $and: [
                             { machine_id: 2 }, { state: 1 }
                         ]
                     },
                         {
-                            $inc: {
-                                runningtime: 1, //Second
-                                operationtime: 1 //Second
-                            }
+                            $set: {
+                                runningtime: Time2, //Second
+                                operationtime: TimeplusO2 //Second
+                            },
                         }, {
                         new: true
                     }
-                    ).sort({ _id: -1 }).then(() => { })
+                    ).sort({ _id: -1 });
                 } else {
                     //-------------------------------DOWNTIME--------------------------------//
+                    TimeplusD2++;
                     await availability.findOneAndUpdate({
                         $and: [
                             { machine_id: 2 }, { state: 1 }
                         ]
                     },
                         {
-                            $inc: {
-                                runningtime: 1, //Second
-                                downtime: 1, //Second
-                            }
+                            $set: {
+                                runningtime: Time2, //Second
+                                downtime: TimeplusD2, //Second
+                            },
                         }, {
                         new: true
                     }
                     ).sort({ _id: -1 }).then(() => { });
                 }
             } else {
+                TimeplusD2 = 0;
+                TimeplusO2 = 0;
                 await availability.updateMany({ machine_id: 2 }, {
                     $set: {
                         state: 0
                     }
-                }).sort({ _id: -1 }).then(() => {
-                    console.log("MESIN 2 SELESAI");
-                })
+                }).sort({ _id: -1 });
+                console.log("MESIN 2 SELESAI");
             }
         } else {
             return null;
@@ -297,45 +306,48 @@ async function OpTimeM3() {
             if (Time3 < (loadingM3 * 60)) {
                 Time3++;
                 if (statusM3 == 1) {
+                    TimeplusO3++
                     await availability.findOneAndUpdate({
                         $and: [
                             { machine_id: 3 }, { state: 1 }
                         ]
                     },
                         {
-                            $inc: {
-                                runningtime: 1, //Second
-                                operationtime: 1 //Second
-                            }
+                            $set: {
+                                runningtime: Time3, //Second
+                                operationtime: TimeplusO3 //Second
+                            },
                         }, {
                         new: true
                     }
-                    ).sort({ _id: -1 }).then(() => { });
+                    ).sort({ _id: -1 });
                 } else {
                     //-------------------------------DOWNTIME--------------------------------//
+                    TimeplusD3++;
                     await availability.findOneAndUpdate({
                         $and: [
                             { machine_id: 3 }, { state: 1 }
                         ]
                     },
                         {
-                            $inc: {
-                                runningtime: 1, //Second
-                                downtime: 1, //Second
-                            }
+                            $set: {
+                                runningtime: Time3, //Second
+                                downtime: TimeplusD3, //Second
+                            },
                         }, {
                         new: true
                     }
                     ).sort({ _id: -1 }).then(() => { });
                 }
             } else {
+                TimeplusD3 = 0;
+                TimeplusO3 = 0;
                 await availability.updateMany({ machine_id: 3 }, {
                     $set: {
                         state: 0
                     }
-                }).sort({ _id: -1 }).then(() => {
-                    console.log("MESIN 3 SELESAI");
-                })
+                }).sort({ _id: -1 });
+                console.log("MESIN 3 SELESAI");
             }
         } else {
             return null;
@@ -364,45 +376,48 @@ async function OpTimeM4() {
             if (Time4 < (loadingM4 * 60)) {
                 Time4++;
                 if (statusM4 == 1) {
+                    TimeplusO4++
                     await availability.findOneAndUpdate({
                         $and: [
                             { machine_id: 4 }, { state: 1 }
                         ]
                     },
                         {
-                            $inc: {
-                                runningtime: 1, //Second
-                                operationtime: 1 //Second
-                            }
+                            $set: {
+                                runningtime: Time4, //Second
+                                operationtime: TimeplusO4 //Second
+                            },
                         }, {
                         new: true
                     }
                     ).sort({ _id: -1 }).then(() => { })
                 } else {
                     //-------------------------------DOWNTIME--------------------------------//
+                    TimeplusD4++;
                     await availability.findOneAndUpdate({
                         $and: [
                             { machine_id: 4 }, { state: 1 }
                         ]
                     },
                         {
-                            $inc: {
-                                runningtime: 1, //Second
-                                downtime: 1, //Second
-                            }
+                            $set: {
+                                runningtime: Time4, //Second
+                                downtime: TimeplusD4, //Second
+                            },
                         }, {
                         new: true,
                     }
-                    ).sort({ _id: -1 }).then(() => { });
+                    ).sort({ _id: -1 });
                 }
             } else {
+                TimeplusD4 = 0;
+                TimeplusO4 = 0;
                 await availability.updateMany({ machine_id: 4 }, {
                     $set: {
                         state: 0
                     }
-                }).sort({ _id: -1 }).then(() => {
-                    console.log("MESIN 4 SELESAI");
-                })
+                }).sort({ _id: -1 });
+                console.log("MESIN 4 SELESAI");
             }
         } else {
             return null;
